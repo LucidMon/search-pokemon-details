0314ca6a7160c841a82bd22cc20c8c22e309df43
LucidMon <ldonm.code@gmail.com>
2023-06-18 03:21:02 -0400

    cambios visuales

--- src/components/PokemonDetail.tsx
+++ src/components/PokemonDetail.tsx
@@ -8,16 +8,16 @@ height: number, weight: number, genera: string, eggGroup: Color[], habitat: stri
     const { name, id, sprites, types, damage, flavor_text_entry, height, weight, genera, eggGroup, habitat, color, jaName, stats } = props;
     return (
         <div className="container">
-            <section className="title-pokemon mx-auto fs-2">
+            <section className="title-pokemon mx-auto fs-1">
                 <span className="name-pokemon">{name}</span>
                 <span className="id-pokemon"> N°{id}</span>
-                <div className="type-pokemon">
+                <div className="type-pokemon fs-5">
                     <ul className="list-inline">
                         {
                             types.map((element) => {
                                 return(
                                     <li className="list-inline-item" key={element.type.name}>
-                                        <div className="card-type">
+                                        <div className={`card-type ${element.type.name}`}>
                                             <span>{element.type.name}</span>
                                         </div>
                                     </li>
@@ -30,121 +30,43 @@ height: number, weight: number, genera: string, eggGroup: Color[], habitat: stri
 
             <div className="row">
                 <div className="col-3">
-                    <section className="pokemon-shiny">
-                        <div className="card border-0">
-                            <img className="card-img-top" src={sprites.other?.["official-artwork"].front_shiny} alt="Pokemon Shiny Img" />
+                    <section className="pokemon-shiny mx-auto">
+                        <span className="shiny-title mx-auto text-center d-block fs-2">Shiny</span>
+                        <div className="shiny-border mx-auto">
+                            <div className="card mx-auto">
+                                <img className="shiny card-img-top" src={sprites.other?.["official-artwork"].front_shiny} alt="Pokemon Shiny Img" />
+                            </div>
                         </div>
                     </section>
                     <section className="pokemon-characteristics">
-                        <div className="card border-0">
-                            <div className="height">
-                                <span>Height</span>
-                                <span>{height/10} m</span>
+                        <div className='card border-0'>
+                            <div className="row">
+                                <div className="col text-center height">
+                                    <span className="fs-5 d-flex justify-content-center">Height</span>
+                                    <p>{height/10} m</p>
+                                </div>
+                                <div className="col text-center weight">
+                                    <span className="fs-5">Weigth</span>
+                                    <p>{weight/10} Kg</p>
+                                </div>
                             </div>
-                            <div className="weight">
-                                <span>Weigth</span>
-                                <span>{weight/10} Kg</span>
-                            </div>  
-                            <div className="genera">
-                                <span>Genera</span>
-                                <span>{genera}</span>
+                            <div className="genera text-center">
+                                <span className="fs-5">Genera</span>
+                                <p>{genera}</p>
                             </div>
                         </div>
                     </section>
                 </div>
 
                 <div className="col-6">
-                    <section className="pokemon-image">
-                        <div className="card border-0">
-                            <img className="card-img-top" alt="Pokemon Img Profile" src={sprites.other?.["official-artwork"].front_default}></img>
-                            <span className="top-right-span fst-italic mx-auto">{jaName}</span>
+                    <section className="pokemon-image mx-auto">
+                        <div className="image-border mx-auto">
+                            <div className="card mx-auto">
+                                <img className="card-img-top" alt="Pokemon Img Profile" src={sprites.other?.["official-artwork"].front_default}></img>
+                            </div>
                         </div>
-                    </section>
-                    <section className="pokemon-stats">
-                        <div className="card border-0">
-                            <span>Stats</span>
-                            <ul className="list-unstyled">
-                                <li>
-                                    <div className="row align-items-center">
-                                        <div className="col-3">
-                                            <span>HP</span>
-                                        </div>
-                                        <div className="col-1">
-                                            <span>{stats[0].base_stat}</span>
-                                        </div>
-                                        <div className="col-7">
-                                            <div className="progress">
-                                                <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[0].base_stat)/135*100}%`, color:'pink'}}></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row align-items-center">
-                                        <div className="col-3">
-                                            <span>Attack</span>
-                                        </div>
-                                        <div className="col-1">
-                                            <span>{stats[1].base_stat}</span>
-                                        </div>
-                                        <div className="col-7">
-                                            <div className="progress">
-                                                <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[1].base_stat)/135*100}%`}}></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row align-items-center">
-                                        <div className="col-3">
-                                            <span>Defense</span>
-                                        </div>
-                                        <div className="col-1">
-                                            <span>{stats[2].base_stat}</span>
-                                        </div>
-                                        <div className="col-7">
-                                            <div className="progress">
-                                                <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[2].base_stat)/135*100}%`}}></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row align-items-center">
-                                        <div className="col-3">
-                                            <span>Special Attack</span>
-                                        </div>
-                                        <div className="col-1">
-                                            <span>{stats[3].base_stat}</span>
-                                        </div>
-                                        <div className="col-7">
-                                            <div className="progress">
-                                                <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[3].base_stat)/135*100}%`}}></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row align-items-center">
-                                        <div className="col-3">
-                                            <span>Special Defense</span>
-                                        </div>
-                                        <div className="col-1">
-                                            <span>{stats[4].base_stat}</span>
-                                        </div>
-                                        <div className="col-7">
-                                            <div className="progress">
-                                                <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[4].base_stat)/135*100}%`}}></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row align-items-center">
-                                        <div className="col-3">
-                                            <span>Speed</span>
-                                        </div>
-                                        <div className="col-1">
-                                            <span>{stats[5].base_stat}</span>
-                                        </div>
-                                        <div className="col-7">
-                                            <div className="progress">
-                                                <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[5].base_stat)/135*100}%`}}></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
+                        <div className="japo mx-auto">
+                            <span className="fst-italic fs-3">{jaName}</span>
                         </div>
                     </section>
                 </div>
@@ -166,6 +88,95 @@ height: number, weight: number, genera: string, eggGroup: Color[], habitat: stri
                     </section>
                 </div>
             </div>
+
+
+            <section className="pokemon-stats mx-auto">
+                <div className="card border-0 ">
+                    <span>Stats</span>
+                    <ul className="list-unstyled">
+                        <li>
+                            <div className="row align-items-center">
+                                <div className="col-3">
+                                    <span>HP</span>
+                                </div>
+                                <div className="col-1">
+                                    <span>{stats[0].base_stat}</span>
+                                </div>
+                                <div className="col-7">
+                                    <div className="progress">
+                                        <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[0].base_stat)/220*100}%`, color:'pink'}}></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-3">
+                                    <span>Attack</span>
+                                </div>
+                                <div className="col-1">
+                                    <span>{stats[1].base_stat}</span>
+                                </div>
+                                <div className="col-7">
+                                    <div className="progress">
+                                        <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[1].base_stat)/220*100}%`}}></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-3">
+                                    <span>Defense</span>
+                                </div>
+                                <div className="col-1">
+                                    <span>{stats[2].base_stat}</span>
+                                </div>
+                                <div className="col-7">
+                                    <div className="progress">
+                                        <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[2].base_stat)/220*100}%`}}></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-3">
+                                    <span>Special Attack</span>
+                                </div>
+                                <div className="col-1">
+                                    <span>{stats[3].base_stat}</span>
+                                </div>
+                                <div className="col-7">
+                                    <div className="progress">
+                                        <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[3].base_stat)/220*100}%`}}></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-3">
+                                    <span>Special Defense</span>
+                                </div>
+                                <div className="col-1">
+                                    <span>{stats[4].base_stat}</span>
+                                </div>
+                                <div className="col-7">
+                                    <div className="progress">
+                                        <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[4].base_stat)/220*100}%`}}></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-3">
+                                    <span>Speed</span>
+                                </div>
+                                <div className="col-1">
+                                    <span>{stats[5].base_stat}</span>
+                                </div>
+                                <div className="col-7">
+                                    <div className="progress">
+                                        <div className={`progress-bar ${types[0].type.name}`} style={{width: `${(stats[5].base_stat)/220*100}%`}}></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
         </div>
     )
 }
